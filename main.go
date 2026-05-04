ef581981b9e04f5e2de214a6af3c35c7a682714b
72 <72sevenzy2@gmail.com>
2026-05-04 15:23:01 +0500

    test: write small test for current implementation

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/72sevenzy2/in-memory-database/db"
+)
 
 func main() {
+	b := *db.NewDB()
+	b.Set("name", "sam")
 
+	val, ok := b.Get("name")
+	if !ok {
+		fmt.Println("not found", val)
+	}
+	fmt.Println(val)
 }
