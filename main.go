61817e7ede3490c511e088d95512130ce300281b
72 <72sevenzy2@gmail.com>
2026-05-04 14:43:35 +0500

    feat: finish initial skeleton

--- main.go
+++ main.go
@@ -4,6 +4,28 @@ import (
 	"fmt"
 )
 
+type DB struct {
+	data map[string]string
+}
+
+func NewDB() *DB {
+	return &DB{
+		data: make(map[string]string),
+	}
+}
+
+func (v *DB) Set(key string, value string) {
+	v.data[key] = value
+}
+
+func (v *DB) Get(key string) (string, bool) {
+	val, ok := v.data[key]
+	if !ok {
+		return "", false
+	}
+	return val, true
+}
+
 func main() {
 	fmt.Println("#")
 }
