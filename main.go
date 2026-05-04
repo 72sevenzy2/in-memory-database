79ff4fb4939e1d5679fb65593d6079771909e0da
72 <72sevenzy2@gmail.com>
2026-05-04 18:04:47 +0500

    feat: add cli usage

--- main.go
+++ main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/72sevenzy2/in-memory-database/db"
 )
 
 func main() {
-	b := *db.NewDB()
-	b.Set("name", "sam")
-	b.Del("name")
+	b := db.NewDB()
 
-	val, ok := b.Get("name")
-	if !ok {
-		fmt.Println("not found", val)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	parts := strings.Split(scanner.Text(), " ")
+
+	switch parts[0] {
+	case "SET":
+		b.Set(parts[1], parts[2])
+
+	case "GET":
+		val, ok := b.Get(parts[1])
+		if ok {
+			fmt.Println(val)
+		} else {
+			fmt.Println("key not found")
+		}
 	}
-	fmt.Println(val)
 }
