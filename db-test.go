564f5df0e014484afe55258f73294c89585b2d65
72 <72sevenzy2@gmail.com>
2026-05-04 17:17:34 +0500

    refactor: move test from main.go to db-test.go

---
+++ db-test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/72sevenzy2/in-memory-database/db"
+)
+
+func TestDb() {
+	b := *db.NewDB()
+	b.Set("name", "sam")
+	b.Del("name")
+
+	val, ok := b.Get("name")
+	if !ok {
+		fmt.Println("not found", val)
+	}
+	fmt.Println(val)
+}
