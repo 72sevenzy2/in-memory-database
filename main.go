0d5cff7a8f8403aded7ddb766f9ab7e791230bee
72 <72sevenzy2@gmail.com>
2026-05-06 16:41:12 +0500

    feat/refactor: improve input handling

--- main.go
+++ main.go
@@ -22,19 +22,19 @@ func main() {
 		input := scanner.Text()
 		parts := strings.Split(input, " ")
 
-		switch parts[0] {
-		case "SET":
+		if parts[0] == "SET" || parts[0] == "set" {
 			b.Set(parts[1], parts[2])
 			fmt.Println("successful")
-
-		case "GET":
+		}
+		if parts[0] == "GET" || parts[0] == "get" {
 			val, ok := b.Get(parts[1])
 			if !ok {
 				fmt.Println("key does not exist")
+			} else {
+				fmt.Println(val)
 			}
-			fmt.Println(val)
-
-		case "EXIT":
+		}
+		if parts[0] == "EXIT" || parts[0] == "exit" {
 			fmt.Println("exited program")
 			return
 		}
