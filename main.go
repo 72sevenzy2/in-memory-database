530bb29691873e5d15397db5bbe195c5d8a2517b
72 <72sevenzy2@gmail.com>
2026-05-06 16:57:07 +0500

    refactor: optimise cli (replace if statements with switch) and more efficient input handling

--- main.go
+++ main.go
@@ -11,6 +11,16 @@ import (
 
 // cli usage
 
+// reusable func to display input format (SET and GET)
+
+func displaySet() {
+	fmt.Println("usage: SET <KeyName> <value>")
+}
+
+func displayGet() {
+	fmt.Println("usage: GET <KeyName>")
+}
+
 func main() {
 	b := db.NewDB()
 
@@ -20,23 +30,44 @@ func main() {
 		scanner.Scan()
 
 		input := scanner.Text()
-		parts := strings.Split(input, " ")
+		parts := strings.Fields(input)
 
-		if parts[0] == "SET" || parts[0] == "set" {
+		if len(parts) == 0 {
+			continue
+		}
+
+		UCinput := strings.ToUpper(parts[0])
+
+		switch UCinput {
+		case "SET":
+			if len(parts) < 3 {
+				fmt.Println("invalid SET format:")
+				displaySet()
+				continue
+			}
+
 			b.Set(parts[1], parts[2])
 			fmt.Println("successful")
-		}
-		if parts[0] == "GET" || parts[0] == "get" {
+		case "GET":
+			if len(parts) < 2 {
+				fmt.Println("invalid GET format:")
+				displayGet()
+				continue
+			}
+
 			val, ok := b.Get(parts[1])
 			if !ok {
 				fmt.Println("key does not exist")
 			} else {
 				fmt.Println(val)
 			}
-		}
-		if parts[0] == "EXIT" || parts[0] == "exit" {
+		case "EXIT":
 			fmt.Println("exited program")
 			return
+
+		default:
+			fmt.Println("invalid command")
 		}
 	}
+
 }
