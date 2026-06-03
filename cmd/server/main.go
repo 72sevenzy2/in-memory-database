ea564e41a65e2d81a4aa9e2a49ce5d3151c23c82
72 <72sevenzy2@gmail.com>
2026-06-03 17:55:19 +0500

    feat: finish initial tcp server.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 func main() {
+	ln, err := net.Listen("tcp", ":8080");
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+
+		if err != nil {
+			fmt.Println(err.Error()) // err acceping connections
+			continue
+		}
+
+		go func(c net.Conn) {
+			defer c.Close() // close connection after reading
+
+			buf := make([]byte, 1024) // preallocated buffer
+
+			n, err := c.Read(buf)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			fmt.Println("received:", string(buf[:n]))
+
+		}(conn)
 	
+	}
+
 }
