d084dd11b8c0cd441a3de3c26fde5a803aabc8c0
72 <72sevenzy2@gmail.com>
2026-06-14 14:56:08 +0500

    refactor: move SET cmd to tcp server.

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"net"
 	"os"
-	"strconv"
 	"strings"
 
 	usa "github.com/72sevenzy2/in-memory-database"
@@ -49,39 +47,6 @@ func main() {
 		}
 
 		switch UCinput {
-		case "SET":
-			if len(parts) < 3 || len(parts) > 3 {
-				fmt.Println("invalid SET format:")
-				usa.DisplaySet()
-				continue
-			}
-
-			f, err := strconv.ParseUint(parts[2], 10, 32) // returns uint64, err.
-
-			if err == nil { // its a int.
-
-				// prevent f from overflowing if number entered is too big
-				if f > math.MaxUint32 {
-					fmt.Println("please include a number value less than unsigned int32.")
-					continue
-				}
-
-				err := b.SetInt(parts[1], uint32(f))
-				if err != nil {
-					fmt.Println(err.Error())
-					continue
-				}
-				fmt.Println("successful.")
-				continue
-			}
-			// its a string if unable to parse to uint.
-			err2 := b.SetString(parts[1], parts[2])
-			if err2 != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			fmt.Println("successful.")
-			continue
 
 		case "GET":
 			if len(parts) < 2 || len(parts) > 2 {
