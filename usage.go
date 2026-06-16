05469ac46ef7fab8128a1534f962a69d00e39bd2
72 <72sevenzy2@gmail.com>
2026-06-16 12:30:47 +0500

    fix: add newline after writing to conn.

--- usage.go
+++ usage.go
@@ -7,13 +7,13 @@ import (
 // reusable functions to display input format (SET and GET)
 
 func DisplaySet(conn net.Conn) {
-	conn.Write([]byte("usage: SET <KeyName> <value>"));
+	conn.Write([]byte("usage: SET <KeyName> <value>\n"));
 }
 
 func DisplayGet(conn net.Conn) {
-	conn.Write([]byte("usage: GET <KeyName>"))
+	conn.Write([]byte("usage: GET <KeyName>\n"))
 }
 
 func DisplayDel(conn net.Conn) {
-	conn.Write([]byte("usage: DEL <KeyName>"))
+	conn.Write([]byte("usage: DEL <KeyName>\n"))
 }
