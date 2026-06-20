ab7289bc748e1a86468972fb743107c73c267176
72 <72sevenzy2@gmail.com>
2026-06-20 17:57:49 +0500

    refactor: combine all usages into 1 func for HELP cmd use in server.

--- usage.go
+++ usage.go
@@ -6,14 +6,10 @@ import (
 
 // reusable functions to display input format (SET and GET)
 
-func DisplaySet(conn net.Conn) {
-	conn.Write([]byte("usage: SET <KeyName> <value>\n"));
-}
-
-func DisplayGet(conn net.Conn) {
-	conn.Write([]byte("usage: GET <KeyName>\n"))
-}
-
-func DisplayDel(conn net.Conn) {
-	conn.Write([]byte("usage: DEL <KeyName>\n"))
+func DisplayUsage(conn net.Conn) {
+	conn.Write([]byte(`
+		SET <KeyName> <value>
+		GET <KeyName>
+		DEL <KeyName>
+	\n`))
 }
